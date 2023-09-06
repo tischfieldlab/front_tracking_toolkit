879a8939649dabdd17953c7ec294b7d83fdf0b9c
quantumdot <quantumdot@users.noreply.github.com>
2023-09-06 11:56:02 -0400

    improve sanity checks output

--- front_tracking_toolkit/sanity.py
+++ front_tracking_toolkit/sanity.py
@@ -2,7 +2,15 @@ import pandas as pd
 
 
 def check_image_metadata_within_sample(img_meta: pd.DataFrame):
-    ignore = ['AcquiredDate', 'CreationDate', 'index']
+    # fields which we expect to change from one image to the next, within an image series
+    ignore = [
+        'AcquiredDate',
+        'CreationDate',
+        'index',
+        'filename',
+        'AcquiredDelta'
+    ]
+    print("I will check each sample for internal consistancy...")
     for group, rows in img_meta.groupby(['subject', 'stain']):
         last = None
         for _, row in rows.iterrows():
@@ -15,16 +23,29 @@ def check_image_metadata_within_sample(img_meta: pd.DataFrame):
                     continue
 
                 if last[key] != row[key]:
-                    print(f'In sample {group}, key {key} changed from "{last[key]}" at index {last["index"]} to "{row[key]}" at index {row["index"]}')
+                    print(f' -> WARN: In sample {group}, key "{key}" changed from "{last[key]}" at index {last["index"]} to "{row[key]}" at index {row["index"]}')
 
             last = row.to_dict()
 
 
 def check_image_metadata_across_samples(img_meta: pd.DataFrame):
-    ignore = ['AcquiredDate', 'CreationDate', 'index', 'XMetresPerPixel', 'YMetresPerPixel']
+    # fields which we expect to change from one image to the next, within and across image series
+    ignore = [
+        'AcquiredDate',
+        'CreationDate',
+        'index',
+        'XMetresPerPixel',
+        'YMetresPerPixel',
+        'filename',
+        'AcquiredDelta',
+        'subject',
+    ]
+    print("I will check across samples for consistancy...")
     for stain, rows in img_meta.groupby('stain'):
+        print(f'Checking stain "{stain}..."')
         for c in rows.columns:
             if c not in ignore:
                 uniq = rows[c].unique()
-                print(c, len(uniq), uniq)
+                if len(uniq) > 1:
+                    print(f' -> WARN: Found {len(uniq)} distinct values for "{c}": {uniq}')
 
